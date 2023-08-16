d3951ee5ecb4e9dfe8c78321bf79c0dd3f8ba4ec
davidRamirez1005 <ramirezjd1005@gmail.com>
2023-08-16 17:18:40 -0500

    feat: :sparkles: prueba de token
    con coleccion eje

--- src/app.js
+++ src/app.js
@@ -4,15 +4,28 @@ import {servidor} from './config/connect.js';
 import {limitget} from './helpers/configLimit.js'
 import { appToken, appVerify } from "./jwt/configToken.js";
 import appUsuarios from './routers/usuariosRouter.js';
-
+import passportHelper from './helpers/passportHelpert.js';
+import { crearToken } from './Auth/middlewareJWT.js';
+import validarPermisos from './jwt/permisosRoles.js';
 
 dotenv.config();
 const appExpress = express();
-
+appExpress.use(passportHelper.initialize());
 
 appExpress.use(express.json());
 
-appExpress.use('/token',limitget(),appToken)
+// Middleware para validar permisos de roles
+
+appExpress.get('/api/admin', passportHelper.authenticate('bearer', { session: false }), validarPermisos, (req, res) => {
+    res.json({mensaje: 'Hola admin', usuario: req.user});
+});
+appExpress.get('/api/vendedor', passportHelper.authenticate('bearer', { session: false }), validarPermisos, (req, res) => {
+    res.json({mensaje: 'Hola vendedor', usuario: req.user});
+});
+
+
+
+appExpress.use('/token/:eje',limitget(),crearToken)
 appExpress.use('/usuario',limitget(), appVerify, appUsuarios)
 
 
