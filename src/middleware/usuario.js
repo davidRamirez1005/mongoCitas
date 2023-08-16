6921593a63f8184c7ec75bc4778ff62da03eddc3
davidRamirez1005 <ramirezjd1005@gmail.com>
2023-08-16 13:37:59 -0500

    feat: :safety_vest: valida si el token corresponde a la coleccion

---
+++ src/middleware/usuario.js
@@ -0,0 +1,37 @@
+import 'reflect-metadata';
+import {plainToClass, classToPlain} from 'class-transformer';
+import { User } from "../routers/storage/usuario.js";
+import { con } from "../../db/atlas.js";
+import {validate} from 'class-validator';
+import { Router } from "express";
+
+const middlewareUsuarioVerify = Router();
+const appDTOData = Router();
+
+let db = await con();
+let collection = db.collection("usuario");
+
+middlewareUsuarioVerify.use((req,res,next) => {
+    if(!req.rateLimit) return; 
+    let {payload} = req.data;
+    const { iat, exp, ...newPayload } = payload;
+    payload = newPayload;
+    let Clone = JSON.stringify(classToPlain(plainToClass(User, {}, { ignoreDecorators: true })));
+    let Verify = Clone === JSON.stringify(payload);
+    (!Verify) ? res.status(406).send({status: 406, message: "No Autorizado"}) : next();  
+});
+appDTOData.use( async(req,res,next) => {
+    try {
+        let data = plainToClass(User, req.body);
+        await validate(data);
+        req.body = JSON.parse(JSON.stringify(data));
+        req.data = undefined;
+        next();
+    } catch (err) {
+        res.status(err.status).send(err)
+    }
+});
+export {
+    middlewareUsuarioVerify,
+    appDTOData
+};
