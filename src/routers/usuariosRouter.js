d54e63bac57f2d660792b88fd4c39942a053cbe9
davidRamirez1005 <ramirezjd1005@gmail.com>
2023-08-16 13:39:15 -0500

    feat: :sparkles: router de usuario
    metodo get

---
+++ src/routers/usuariosRouter.js
@@ -0,0 +1,55 @@
+import {Router} from 'express';
+import dotenv from 'dotenv';
+import {con} from '../../db/atlas.js'
+import { middlewareUsuarioVerify, appDTOData } from '../middleware/usuario.js';
+import siguienteId from '../helpers/siguienteId.js';
+
+dotenv.config();
+
+const appUsuarios = Router();
+
+/**
+ * ! GET
+ * ? listar todas las bodegas ordenadas
+ * * http://127.0.0.3:5012/usuario/listar
+ */
+appUsuarios.get('/listar', middlewareUsuarioVerify, async(req, res) =>{
+    if(!req.rateLimit) return;
+
+    let db = await con();
+    let coleccion = db.collection('usuario');
+    let result = await coleccion.find().sort({ nombre: 1 }).project({_id : 0}).toArray();
+    res.send(result)
+})
+
+/**
+* ! POST
+* ? agregar una nueva bodega
+* * http://127.0.0.3:5012/usuario
+*/
+appUsuarios.post('/', middlewareUsuarioVerify, appDTOData, async (req, res) => {
+    if (!req.rateLimit) return;
+
+    try {
+        const newId = await siguienteId( "usuario");
+
+        let db = await con();
+        let coleccion = db.collection('usuario');
+        
+        const newDocument = {
+            _id: newId,
+            ...req.body
+        };
+        let result = await coleccion.insertOne(newDocument);
+        console.log(result);
+        res.status(201).send({ status: 201, message: 'documento creado con exito' });
+    } catch (error) {
+        console.log(error);
+        res.status(406).send('no se ha podido crear el documento');
+    }
+});
+
+
+
+
+export default appUsuarios;
