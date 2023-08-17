4a2656226e9eac16fbc5c95d3a5ccfe8d17a3aa4
davidRamirez1005 <ramirezjd1005@gmail.com>
2023-08-17 16:30:13 -0500

    feat: :sparkles: enpoints

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ import express from 'express';
 import dotenv from 'dotenv';
 import {servidor} from './config/connect.js';
 import {limitget} from './helpers/configLimit.js'
-import { appToken, appVerify } from "./jwt/configToken.js";
 import appUsuarios from './routers/usuariosRouter.js';
 import passportHelper from './helpers/passportHelpert.js';
 import { crearToken } from './Auth/middlewareJWT.js';
@@ -17,16 +16,16 @@ appExpress.use(express.json());
 // Middleware para validar permisos de roles
 
 appExpress.get('/api/admin', passportHelper.authenticate('bearer', { session: false }), validarPermisos, (req, res) => {
-    res.json({mensaje: 'Hola admin', usuario: req.user});
+    res.json({mensaje: 'Hola admin', usuario : req.user});
 });
 appExpress.get('/api/vendedor', passportHelper.authenticate('bearer', { session: false }), validarPermisos, (req, res) => {
-    res.json({mensaje: 'Hola vendedor', usuario: req.user});
+    res.json({mensaje: 'Hola vendedor'});
 });
 
 
 
-appExpress.use('/token/:eje',limitget(),crearToken)
-appExpress.use('/usuario',limitget(), appVerify, appUsuarios)
+appExpress.use('/token/:usuario',limitget(),crearToken)
+appExpress.use('/usuario',limitget(), appUsuarios)
 
 
 
