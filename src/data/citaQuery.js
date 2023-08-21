cc5392a875ca07cf5cd16692b4e426178c898667
davidRamirez1005 <ramirezjd1005@gmail.com>
2023-08-21 11:32:48 -0500

    feat: :sparkles: consultas a la base de datos
    para exportarla a la logica del enpoint

--- src/data/citaQuery.js
+++ src/data/citaQuery.js
@@ -9,14 +9,63 @@ export const fecha = [
         }
     }
 ];
+let generoConsultado = "Masculino";
 export const generoAtendido = [
     {
-        $match: { usu_genero: 'Masculino' }
+        $lookup: {
+            from: "usuario",
+            localField: "cit_datosUsuario",
+            foreignField: "_id",
+            as: "datosUsuario"
+        }
     },
     {
-        $project : {
-            _id : 0
-            
+        $match: {
+            "datosUsuario.usu_genero": generoConsultado,
+            "cit_estado": "Realizada"
+        }
+    },
+    {
+        $project: {
+            _id: 0,
+            "cita": "$$ROOT"
         }
     }
 ];
+let mesEspecifico = 7;
+export const rechazadaCita = [
+    {
+        $match: {
+        cit_estado: "Rechazada",
+        cit_fecha: {
+            $gte: new Date(`2023-${mesEspecifico}-01`),
+            $lt: new Date(`2023-${mesEspecifico + 1}-01`)
+        }
+        }
+    },
+    {
+        $lookup: {
+        from: "usuario",
+        localField: "cit_datosUsuario",
+        foreignField: "_id",
+        as: "usuario"
+        }
+    },
+    {
+        $lookup: {
+        from: "medico",
+        localField: "cit_medico",
+        foreignField: "_id",
+        as: "medico"
+        }
+    },
+    {
+        $project: {
+        _id: 0,
+        cit_fecha: 1,
+        "usuario.usu_nombre": 1,
+        "usuario.usu_primer_apellido": 1,
+        "medico.med_nombreCompleto": 1
+        }
+    }
+    ];
