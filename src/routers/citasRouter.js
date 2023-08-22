abf1e36b786676bee432014593985629b77ee169
davidRamirez1005 <ramirezjd1005@gmail.com>
2023-08-22 17:32:38 -0500

    feat: :sparkles: se ikmporta validacion de token

--- src/routers/citasRouter.js
+++ src/routers/citasRouter.js
@@ -1,41 +1,42 @@
 import {Router} from 'express';
 import dotenv from 'dotenv';
 import { listar, idEsp, diaEspe, citaGenero, citaRechazadaMes } from '../controllers/citasController.js'
+import validarPermisos from '../Auth/permisosRoles.js'
 
 dotenv.config();
 
 const appCitas = Router();
-
+appCitas.use(validarPermisos)
 /**
  * ! GET
  * ? Obtener todas las citas alfabéticamente
- * * http://127.0.0.3:5012/cita/listar
+ * * http://127.0.0.3:5012/cita/cita/listar
  */
 appCitas.get('/listar',listar)
 /**
  * ! GET
  * ? Encontrar todas las citas para un día específico (por ejemplo, '2023-07-19')
- * * http://127.0.0.3:5012/cita/diaEsp
+ * * http://127.0.0.3:5012/cita/cita/diaEsp
  * ! terminar
  */
-appCitas.get('/diaEsp', diaEspe);
+appCitas.get('/cita/diaEsp', diaEspe);
 /**
  * ! GET
  * ? Mostrar todas las citas que fueron rechazadas y en un mes específico, mostrar la fecha de la cita, el nombre del usuario y el médico.
- * * http://127.0.0.3:5012/cita/citaRechazada
+ * * http://127.0.0.3:5012/cita/cita/citaRechazada
  */
-appCitas.get('/citaRechazada', citaRechazadaMes);
+appCitas.get('/cita/citaRechazada', citaRechazadaMes);
 /**
  * ! GET
  * ? Obtener todas las citas realizadas por los pacientes de un genero si su estado de la cita fue realizada
- * * http://127.0.0.3:5012/cita/pacienteGenero
+ * * http://127.0.0.3:5012/cita/cita/pacienteGenero
  */
-appCitas.get('/pacienteGenero', citaGenero);
+appCitas.get('/cita/pacienteGenero', citaGenero);
 /**
  * ! GET
  * ? Encontrar la próxima cita para un paciente específico (por ejemplo, el paciente con usu_id 1):
- * * http://127.0.0.3:5012/cita/1
+ * * http://127.0.0.3:5012/cita/cita/1
  */
-appCitas.get('/:citaId', idEsp);
+appCitas.get('/cita/:citaId', idEsp);
 
 export default appCitas;
--- src/routers/usuariosRouter.js
+++ src/routers/usuariosRouter.js
@@ -1,34 +1,36 @@
 import {Router} from 'express';
 import dotenv from 'dotenv';
 import { listar, Paciente_medico, pacientes, insertarPaciente } from '../controllers/usuariosController.js'
+import validarPermisos from '../Auth/permisosRoles.js'
 
 dotenv.config();
 
 const appUsuarios = Router();
+appUsuarios.use(validarPermisos)
 
 /**
  * ! GET
  * ? listar todas los usuarios ordenados alfabeticamente
- * * http://127.0.0.3:5012/usuario/listar
+ * * http://127.0.0.3:5012/usuario/usuario/listar
  */
-appUsuarios.get('/listar',listar)
+appUsuarios.get('/usuario/listar',listar)
 /**
  * ! GET
  * ? Encontrar todos los pacientes que tienen citas con un médico específico (por ejemplo, el médico con _id 1)
- * * http://127.0.0.3:5012/usuario/medico/2
+ * * http://127.0.0.3:5012/usuario/usuario/medico/2
  */
-appUsuarios.get('/medico/:medicoId',Paciente_medico)
+appUsuarios.get('/usuario/medico/:medicoId',Paciente_medico)
 /**
  * ! GET
  * ? Obtener las consultorías para un paciente específico (por ejemplo, paciente con usu_id 1)
- * * http://127.0.0.3:5012/usuario/consultoria/2
+ * * http://127.0.0.3:5012/usuario/usuario/consultoria/2
  */
-appUsuarios.get('/consultoria/:usuarioId',pacientes)
+appUsuarios.get('/usuario/consultoria/:usuarioId',pacientes)
 /**
  * ! POST
  * ? Insertar un paciente a la tabla usuario pero si es menor de edad solicitar primero que ingrese el acudiente y validar si ya estaba registrado el acudiente.
- * * http://127.0.0.3:5012/usuario
+ * * http://127.0.0.3:5012/usuario/usuario
  */
-appUsuarios.post('/',insertarPaciente)
+appUsuarios.post('/usuario',insertarPaciente)
 
 export default appUsuarios;
