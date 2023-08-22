bb2850d69b464afa38bc9e2cd162c85e7d310bf3
davidRamirez1005 <ramirezjd1005@gmail.com>
2023-08-22 17:32:54 -0500

    feat: :sparkles: enpoint de login

--- src/app.js
+++ src/app.js
@@ -2,34 +2,25 @@ import express from 'express';
 import dotenv from 'dotenv';
 import {servidor} from './config/connect.js';
 import {limitget} from './config/configLimit.js'
-import passportHelper from './helpers/passportHelpert.js';
-import { crearToken } from './jwt/configJwt.js';
-import validarPermisos from './Auth/permisosRoles.js';
+import passport from './helpers/passportHelpert.js';
+import { generateToken } from './jwt/token.js';
 import appUsuarios from './routers/usuariosRouter.js';
 import appCitas from './routers/citasRouter.js';
 import appMedico from './routers/medicosRouter.js';
 
 dotenv.config();
 const appExpress = express();
-appExpress.use(passportHelper.initialize());
+appExpress.use(passport.initialize());
 
 appExpress.use(express.json());
 
-// Middleware para validar permisos de roles
 
-appExpress.get('/api/admin', passportHelper.authenticate('bearer', { session: false }), validarPermisos, (req, res) => {
-    res.json({mensaje: 'Hola admin', usuario : req.user});
-});
-appExpress.get('/api/vendedor', passportHelper.authenticate('bearer', { session: false }), validarPermisos, (req, res) => {
-    res.json({mensaje: 'Hola vendedor'});
-});
+appExpress.use(limitget())
 
-
-
-appExpress.use('/token/:usuario',limitget(),crearToken)
-appExpress.use('/usuario',limitget(), appUsuarios)
-appExpress.use('/cita',limitget(), appCitas)
-appExpress.use('/medico',limitget(), appMedico)
+appExpress.use('/login', generateToken)
+appExpress.use('/usuario', passport.authenticate('bearer', { session: false }),appUsuarios)
+appExpress.use('/cita', passport.authenticate('bearer', { session: false }), appCitas)
+appExpress.use('/medico', passport.authenticate('bearer', { session: false }), appMedico)
 
 
 
