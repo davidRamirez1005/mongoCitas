1f6aa474349c0c268dab1ecc375d1cc1f0d88b46
davidRamirez1005 <ramirezjd1005@gmail.com>
2023-10-25 13:04:57 -0500

    refactor: :fire: se eliminan archivos que no son necesarios porque se cambia la estructura del proyecto

--- api/server/infraestructure/rutas.js
+++ api/server/infraestructure/rutas.js
@@ -1,13 +1,13 @@
-// import passport from '../../Auth/app/passportService.js';
+import passport from '../../Auth/app/passportService.js';
 import { generateToken } from '../../Auth/infraestructure/jwt/generateJwt.js';
-// import appUsuarios from '../../routers/usuariosRouter.js';
-// import appCitas from '../../routers/citasRouter.js';
-// import appMedico from '../../routers/medicosRouter.js';
+import appUsuarios from '../../usuario/app/usuariosRouter.js';
+import appCitas from '../../citas/app/citasRouter.js'
+import appMedico from '../../medico/app/medicosRouter.js';
 
 export function routesRepository(nameExpress) {
     nameExpress.use('/login', generateToken)
+    nameExpress.use('/cita', passport.authenticate('bearer', { session: false }), appCitas)
+    nameExpress.use('/medico', passport.authenticate('bearer', { session: false }), appMedico)
+    nameExpress.use('/usuario', passport.authenticate('bearer', { session: false }),appUsuarios)
 }
-// appExpress.use('/usuario', passport.authenticate('bearer', { session: false }),appUsuarios)
-// appExpress.use('/cita', passport.authenticate('bearer', { session: false }), appCitas)
-// appExpress.use('/medico', passport.authenticate('bearer', { session: false }), appMedico)
 
